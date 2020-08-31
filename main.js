e20241eb8e458d5765ff10784515ffe595b7f735
mkuppu1978 <MKUPPU1978@GMAIL.COM>
2020-08-31 15:55:55 +0000

    Lab 4

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 /*
+  Import the ip-cidr npm package.
+  See https://www.npmjs.com/package/ip-cidr
+  The ip-cidr package exports a class.
+  Assign the class definition to variable IPCIDR.
+*/
+const IPCIDR = require('ip-cidr');
+
+/*
   Import the built-in path module.
   See https://nodejs.org/api/path.html
   The path module provides utilities for working with file and directory paths.
@@ -14,23 +22,6 @@ const path = require('path');
  * to unequivocally locate the file module.
  */
 const { getIpv4MappedIpv6Address } = require(path.join(__dirname, 'ipv6.js'));
-
-
-/*
-  Import the ip-cidr npm package.
-  See https://www.npmjs.com/package/ip-cidr
-  The ip-cidr package exports a class.
-  Assign the class definition to variable IPCIDR.
-*/
-const IPCIDR = require('ip-cidr');
-const util = require('util')
-
-/**
-* @typedef {Object} IpAddress
-* @property {string} ipv4 - holds ipv4 address
-* @property {string} ipv6 - holds ipv6 address
-*/
-
 class IpAddress {
   constructor() {
     // IAP's global log object is used to output errors, warnings, and other
@@ -41,22 +32,21 @@ class IpAddress {
     log.info('Starting the IpAddress product.');
   }
   /**
-* Calculate and return the first host IP address from a CIDR subnet.
-* @param {string} cidrStr - The IPv4 subnet expressed
-*                 in CIDR format.
-* @param {callback} callback - A callback function.
-* @return {IpAddress} - Retunrs the object with ipv4 and ipv6 strings
-*/
-getFirstIpAddress(cidrStr, callback) {
+ * Calculate and return the first host IP address from a CIDR 
+ subnet in IPv6 and IPv4 forms.
+ * @param {string} cidrStr - The IPv4 subnet expressed
+ *                 in CIDR format.
+ * @param {callback} callback - A callback function.
+ * @return {string} (firstIpAddress) - An object containing an
+ *                 IPv6 and IPv4 address.
+ */
+ getFirstIpAddress(cidrStr, callback) {
 
   // Initialize return arguments for callback
   let firstIpAddress = null;
   let callbackError = null;
-  var IpAddress = {
-            ipv4: null,
-            ipv6: null
-        };
-  
+  let IPv6Address = null;
+
   // Instantiate an object from the imported class and assign the instance to variable cidr.
   const cidr = new IPCIDR(cidrStr);
   // Initialize options for the toArray() method.
@@ -75,15 +65,13 @@ getFirstIpAddress(cidrStr, callback) {
     // If the passed CIDR is valid, call the object's toArray() method.
     // Notice the destructering assignment syntax to get the value of the first array's element.
     [firstIpAddress] = cidr.toArray(options);
-    let ipv6Addr = getIpv4MappedIpv6Address(firstIpAddress);
-    IpAddress.ipv6 = ipv6Addr;
-    IpAddress.ipv4 = firstIpAddress;
+    IPv6Address = getIpv4MappedIpv6Address(firstIpAddress)
   }
   // Call the passed callback function.
   // Node.js convention is to pass error data as the first argument to a callback.
   // The IAP convention is to pass returned data as the first argument and error
   // data as the second argument to the callback function.
-  return callback(IpAddress, callbackError);
+  return callback({ipv4: firstIpAddress, ipv6: IPv6Address}, callbackError);
 }
 }
-module.exports.IpAddress = new IpAddress;
+module.exports = new IpAddress;
